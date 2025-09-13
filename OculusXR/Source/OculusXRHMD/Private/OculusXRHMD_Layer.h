e41cd2644d0ac8516bfeee09a0acaaf784cd89fa
truefranco <truefranco@yahoo.com>
2025-09-13 12:49:07 -0400

    Update OculusXRHMD_Layer.h

--- OculusXR/Source/OculusXRHMD/Private/OculusXRHMD_Layer.h
+++ OculusXR/Source/OculusXRHMD/Private/OculusXRHMD_Layer.h
@@ -65,7 +65,7 @@ namespace OculusXRHMD
 		bool ShapeNeedsTextures(ovrpShape shape);
 
 		PRAGMA_DISABLE_DEPRECATION_WARNINGS
-			FTextureRHIRef GetTexture() { return Desc.TextureObj->GetResource()->GetTextureReference(); }
+			FTextureRHIRef GetTexture() { return Desc.LeftTexture; }
 		PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 		TSharedPtr<FLayer, ESPMode::ThreadSafe> Clone() const;
